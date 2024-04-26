a34e6cb2652ed2d7f176f6e756fbe05931cac84b
Rodrigo Zuñiga <rodrigo.zuniga@subdere.gov.cl>
2024-04-26 12:17:24 -0400

    feat: Default initial position to Santiago, Chile ... for now

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
+// default latitude to Santiago
+const defaultLongitude = -7865172.184112017;
+const defaultLatitude = -3954066.374903847;
+const defaultZoom = 16;
+
+// this may be a URL parameter
+const params = {};
+
+const latitude = params.latitude ? Number(params.latitude) : defaultLatitude;
+const longitude = params.longitude ? Number(params.longitude) : defaultLongitude;
+const zoom = params.zoom ? Number(params.zoom) : defaultZoom;
+
 const map = new Map({
   target: 'map',
   layers: [
@@ -11,7 +23,7 @@ const map = new Map({
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: [longitude, latitude],
+    zoom: zoom
   })
 });
